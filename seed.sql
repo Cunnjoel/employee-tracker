617b6842173d3ece9efd3690d5a62e0323dd993f
Cunnjoel <cunningham.joel84@gmail.com>
2021-04-30 20:11:16 -0500

    adding seed.sql

---
+++ seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO employee (first_name, last_name, role, title, salary, name)
+VALUES ("Joe", "Shmo", "manager", "Head Engineer", 100,000, "Programing");
+
+INSERT INTO employee (first_name, last_name, role, title, salary, name)
+VALUES ("John", "Doe", "new guy", "Engineer", 60,000, "Programing");
+
+INSERT INTO employee (first_name, last_name, role, title, salary, name)
+VALUES ("Jane", "Doe", "cool lady", "Salery", 80,000, "HR");
